58ab761c2b020e836bad0104032e6ebe3e73e69c
portillapedreda <xrportillapedreda@gmail.com>
2022-10-20 18:38:06 +0200

    script 20 Octubre

---
+++ 3primercursor.sql
@@ -0,0 +1,33 @@
+-- CURSOR
+
+USE AdventureWorks2017
+GO
+DECLARE Employee_Cursor CURSOR FOR
+    SELECT BusinessEntityID, JobTitle
+	FROM AdventureWorks2017.HumanResources.Employee;
+OPEN Employee_Cursor;
+FETCH NEXT FROM Employee_Cursor;
+WHILE @@FETCH_STATUS = 0
+    BEGIN
+	    FETCH NEXT FROM Employee_Cursor
+	END;
+CLOSE Employee_Cursor;
+DEALLOCATE Employee_Cursor;
+
+
+CREATE OR ALTER PROC EMPLOYEE_CURSOR
+AS
+       DECLARE Employee_Cursor CURSOR FOR
+          SELECT BusinessEntityID, JobTitle
+	      FROM AdventureWorks2017.HumanResources.Employee;
+       OPEN Employee_Cursor;
+       FETCH NEXT FROM Employee_Cursor;
+       WHILE @@FETCH_STATUS = 0
+          BEGIN
+	          FETCH NEXT FROM Employee_Cursor
+	      END;
+       CLOSE Employee_Cursor;
+       DEALLOCATE Employee_Cursor;
+GO
+EXEC EMPLOYEE_CURSOR
+GO
